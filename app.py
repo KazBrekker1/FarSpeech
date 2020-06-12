a58160f20ce69977abbc3f2519b395d50d46ef09
KazBrekkerSOC <justanothernerd000@gmail.com>
2020-06-12 08:50:20 +0300

    #Re-initialized an enviroment   -added new library (flask-socketio)

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, send_file
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 
 
 @app.route('/')
@@ -8,5 +11,10 @@ def hello_world():
     return send_file('templates/index.html')
 
 
+@socketio.on('json')
+def handle_json(json):
+    print('received json: ' + str(json))
+
+
 if __name__ == '__main__':
-    app.run()
+    socketio.run(app)
