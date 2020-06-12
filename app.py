4ad6c78ac82d8f36957901e406b40e5bfec692aa
KazBrekkerSOC <justanothernerd000@gmail.com>
2020-06-12 11:26:46 +0300

    #Basic Socket IO Trial In:   -Frontend mob.js file   -Python Flask server app.py file

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, send_file
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
+app.config['SECRET_KEY'] = 'farspeech12345'
 socketio = SocketIO(app)
 
 
@@ -14,6 +14,26 @@ def hello_world():
 @socketio.on('json')
 def handle_json(json):
     print('received json: ' + str(json))
+
+
+@socketio.on('connect')
+def test_connect():
+    print("It Connected")
+
+""" 
+EVENT HANDLER
+handels: my event (from OnResult in the front end)
+"""
+@socketio.on('my event')
+def trial(data):
+    print(data)
+    """ 
+    EVENT EMMITTER
+    event name : event 2 
+    """
+    socketio.emit('event 2', {
+        "data": "Emmited data From Backend"
+    })
 
 
 if __name__ == '__main__':
